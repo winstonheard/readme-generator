d2f96887b243cdc305c4a85b0d5c514da3bb43c1
Winston Heard <winston.nolen@gmail.com>
2023-04-15 13:30:45 -0700

    updating generateMarkdown and index js files

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,72 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    message: "What are the instructions for use?",
+    name: 'usage',
+  },
+  {
+    type: 'list',
+    message: "Which license would you like to use for your project?",
+    choices: ["MIT", "Apache", "GPL", "ISC", 'None'],
+    name: 'license',
+  },
+  {
+    type: 'input',
+    message: "Provide the contribution guidelines for your project:",
+    name: 'contributing',
+  },
+  {
+    type: 'input',
+    message: "Write tests for your project. Include examples for how to run your tests.",
+    name: 'tests'
+  },
+  {
+    type: 'input',
+    message: "What is your github username?",
+    name: 'github'
+  },
+  {
+    type: 'input',
+    message: "What is your email address?",
+    name: 'email'
+  }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log('README.md has been generated successfully.');
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Create a function to initialize app
+async function init() {
+  try {
+    const answers = await inquirer.prompt(questions);
+    const markdown = generateMarkdown(answers);
+    writeToFile('README.md', markdown);
+  } catch (error) {
+    console.error('An error occurred during the README generation:', error);
+  }
+}
 
 // Function call to initialize app
 init();
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,79 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on the selected license
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+  return `![License: ${license}](https://img.shields.io/badge/License-${license.replace(' ', '%20')}-green.svg)`;
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function that returns the license link
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+  return `https://opensource.org/licenses/${license}`;
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function that returns the license section of README
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+  return `
+## License
 
-// TODO: Create a function to generate markdown for README
+This project is licensed under the ${license} License. 
+
+${renderLicenseBadge(license)}
+
+For more information, please visit [${renderLicenseLink(license)}](${renderLicenseLink(license)}).
+`;
+}
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+
+${data.description}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+If you have any questions or need additional information, feel free to contact me:
+
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
 `;
 }
 
